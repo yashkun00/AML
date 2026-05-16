93dc55c692d14438e77bd3b21bd777b2040b5ba5
YASH WASULKAR <145082484+yashkun00@users.noreply.github.com>
2026-05-16 22:26:35 +0530

    Update and rename pandas.md to pandas.py

---
+++ pandas.py
@@ -0,0 +1,12 @@
+import pandas as pd
+from mlxtend.frequent_patterns import apriori
+from mlxtend.frequent_patterns import association_rules
+data = {
+'Transaction': [1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5], 'Item': ['Milk', 'Bread', 'Eggs', 'Bread', 'Diaper', 'Beer', 'Milk', 'Diaper', 'Beer', 'Bread', 'Milk', 'Diaper', 'Bread', 'Milk', 'Eggs']
+}
+df = pd.DataFrame(data)
+basket = df.groupby(['Transaction', 'Item'])['Item'].count().unstack().fillna(0)
+freq_Itemsets = apriori(basket, min_support = 0.2, use_colnames = True)
+rules = association_rules(freq_Itemsets, metric = 'confidence', min_threshold = 0.59)
+print("Association Rules:")
+print(rules)
