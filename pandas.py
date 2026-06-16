a4ebee47337ec3426f7a03f69a6ca182210e36f1
YASH WASULKAR <145082484+yashkun00@users.noreply.github.com>
2026-06-16 22:34:13 +0530

    Fix transaction data in pandas.py

--- pandas.py
+++ pandas.py
@@ -2,7 +2,7 @@ import pandas as pd
 from mlxtend.frequent_patterns import apriori
 from mlxtend.frequent_patterns import association_rules
 data = {
-'Transaction': [1, 1, 2, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5], 'Item': ['Milk', 'Bread', 'Eggs', 'Bread', 'Diaper', 'Beer', 'Milk', 'Diaper', 'Beer', 'Bread', 'Milk', 'Diaper', 'Bread', 'Milk', 'Eggs']
+'Transaction': [1, 1, 1, 2, 2, 2, 3, 3, 4, 4, 4, 5, 5, 5, 5], 'Item': ['Milk', 'Bread', 'Eggs', 'Bread', 'Diaper', 'Beer', 'Milk', 'Diaper', 'Beer', 'Bread', 'Milk', 'Diaper', 'Bread', 'Milk', 'Eggs']
 }
 df = pd.DataFrame(data)
 basket = df.groupby(['Transaction', 'Item'])['Item'].count().unstack().fillna(0)
